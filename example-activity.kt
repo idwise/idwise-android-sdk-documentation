862c7f1461ec83129108ab6084a41f327ffc30fd
waqar-idwise <94914142+waqar-idwise@users.noreply.github.com>
2022-02-03 17:10:11 +0500

    updated for v 3.0.1

--- example-activity.kt
+++ example-activity.kt
@@ -8,22 +8,65 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        
+        /** 
+        * Initialaize the SDK by replacing the "<CLIENT_KEY>" with
+        * your own key provided by IDWIse. error callback will be 
+        * triggered if there is any error in initialization of the SDK
+        */
+        
+        IDWise.initialize("<CLIENT_KEY>") { error: IDWiseSDKError? ->
+            error?.printStackTrace()
+        }
+        
+        btnStartVerification?.setOnClickListener {
+         
+            IDWise.startJourney(
+			context,
+			"<YOUR_JOURNEY_TEMPLATE_ID>", //Provided by IDWise
+			"<REFERENCE_NUMBER>", 
+			"en",
+			object : IDWiseSDKCallback {
+                
+                /** 
+                * This event triggers when the Journey is started by
+                * the user, by clicking on the Start Journey Button
+                */
+			    override fun onJourneyStarted(journeyInfo: JourneyInfo) {
+				    Log.d("IDWiseSDKCallback", "onJourneyStarted")
+			    }
 
-        IDWise.startJourney(this,
-            "Journey Template ID",//provided by IDWise
-            "Customer Specific Reference Number",
-            { journeyId, submitted ->
-                // Journey completed callback.
-            },
-            { journeyId ->
-                // Journey cancelled callback.
-            },
-            { journeyInfo ->
-                // Journey started callback (You can get journey id from journeyInfo?.journeyId
-            },
-            "en" // Locale (language) for UI, we are passing "ar" for Arabic in this example, for supported languages, please contact IDWise.
-                 // Adding more languages (if not supported out of the box) is very simple.
-        )
+                /** 
+                * This event triggers when the Journey is completed by
+                * the user.
+                */
+                
+			    override fun onJourneyCompleted(
+				    journeyInfo: JourneyInfo,
+				    isSucceeded: Boolean
+			    ) {
+				    Log.d("IDWiseSDKCallback", "onJourneyCompleted")
+			    }
+
+                 /** 
+                * This event triggers when the Journey is cancelled by
+                * the user at any stage
+                */
+			    override fun onJourneyCancelled(journeyInfo: JourneyInfo?) {
+				    Log.d("IDWiseSDKCallback", "onJourneyCancelled")
+			    }
+
+                
+                 /** 
+                * This event triggers when an Error occured during the 
+                * verification process
+                */
+			    override fun onError(error: IDWiseSDKError) {
+				    Log.d("IDWiseSDKCallback", "onError ${error.message}")
+			    }
+			})
+            
+        }
     }
 
 }
